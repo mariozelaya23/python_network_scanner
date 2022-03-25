eba5eea0a1a68a91b56c6f566d2991e806bd1e33
Mario Zelaya <mariozelaya23@gmail.com>
2022-03-25 13:11:24 -0500

    refactoring code adding ARP to ask who has target IP, and set dest MAC to broadcast MAC

--- network_scanner.py
+++ network_scanner.py
@@ -1,11 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-import scapy.all as scapy
+from scapy.all import *
 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.all.ARP(pdst=ip)
+    arp_request.show()
+    broadcast = scapy.all.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast.show()
 
 
-scan("192.168.1.1/24")
+scan("192.168.2.1/24")
 
