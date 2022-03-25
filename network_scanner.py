5a9ccbbbd631a652506a5dff31227d8900289700
Mario Zelaya <mariozelaya23@gmail.com>
2022-03-25 18:03:26 -0500

    printing the values in a list instead of a list

--- network_scanner.py
+++ network_scanner.py
@@ -10,8 +10,12 @@ def scan(ip):
     answered_list = scapy.all.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
     print("IP\t\t\tMAC Address\n------------------------------------------------")
+    client_list = []
     for answered_element in answered_list:
-        print(answered_element[1].psrc + "\t\t" + answered_element[1].hwsrc)
+        client_dict = {"ip": answered_element[1].psrc, "mac": answered_element[1].hwsrc}
+        client_list.append(client_dict)
+        # print(answered_element[1].psrc + "\t\t" + answered_element[1].hwsrc)
+    print(client_list)
 
 
 scan("192.168.2.1/24")
