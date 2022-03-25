60b6e6d47321017412ea5fa580e4558343c72b4e
Mario Zelaya <mariozelaya23@gmail.com>
2022-03-25 17:36:00 -0500

    parsing the response (id and mac) printing basically the result

--- network_scanner.py
+++ network_scanner.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
-
+import scapy.all
 from scapy.all import *
 
 
 def scan(ip):
     arp_request = scapy.all.ARP(pdst=ip)
-    arp_request.show()
     broadcast = scapy.all.Ether(dst="ff:ff:ff:ff:ff:ff")
-    broadcast.show()
     arp_request_broadcast = broadcast/arp_request
-    arp_request_broadcast.show()
+    answered_list = scapy.all.srp(arp_request_broadcast, timeout=1)[0]
+    print(answered_list.summary())
+
+    for answered_element in answered_list:
+        print(answered_element[1].psrc)
+        print(answered_element[1].hwsrc)
+        print("--------------------------------------------------------------------------------")
 
 
 scan("192.168.2.1/24")
