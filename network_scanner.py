285782ed69fb4b9a345112aaabd9a3a3224fd179
Mario Zelaya <mariozelaya23@gmail.com>
2022-03-25 17:45:30 -0500

    adding changes to print the result in a better way

--- network_scanner.py
+++ network_scanner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import scapy.all
+
 from scapy.all import *
 
 
@@ -7,13 +7,11 @@ def scan(ip):
     arp_request = scapy.all.ARP(pdst=ip)
     broadcast = scapy.all.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.all.srp(arp_request_broadcast, timeout=1)[0]
-    print(answered_list.summary())
+    answered_list = scapy.all.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
+    print("IP\t\t\tMAC Address\n------------------------------------------------")
     for answered_element in answered_list:
-        print(answered_element[1].psrc)
-        print(answered_element[1].hwsrc)
-        print("--------------------------------------------------------------------------------")
+        print(answered_element[1].psrc + "\t\t" + answered_element[1].hwsrc)
 
 
 scan("192.168.2.1/24")
