282e4e00e8340b96d3fd4e749003b15e32763f3f
Mario Zelaya <mariozelaya23@gmail.com>
2022-03-27 13:42:44 -0500

    refactoring code due to adding optparse module and the respective function

--- network_scanner.py
+++ network_scanner.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 
 from scapy.all import *
+import optparse
+
+
+def get_argument():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target", help="IP range")
+    options, arguments = parser.parse_args()
+    return options
 
 
 def scan(ip):
+    print("[+] Scanning IP " + ip)
     arp_request = scapy.all.ARP(pdst=ip)
     broadcast = scapy.all.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.all.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-
     client_list = []
+
     for answered_element in answered_list:
         client_dict = {"ip": answered_element[1].psrc, "mac": answered_element[1].hwsrc}
         client_list.append(client_dict)
@@ -22,5 +31,7 @@ def print_result(results_list):
         print(client["ip"] + "\t\t" + client["mac"])
 
 
-scan_result = scan("192.168.1.1/24")
+options = get_argument()
+
+scan_result = scan(options.target)
 print_result(scan_result)
