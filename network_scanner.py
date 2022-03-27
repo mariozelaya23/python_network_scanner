6d34c53963c2b10fa7c95e3cb50841b4555791dd
Mario Zelaya <mariozelaya23@gmail.com>
2022-03-27 12:21:56 -0500

    adding a function for print the dictionary values

--- network_scanner.py
+++ network_scanner.py
@@ -9,14 +9,18 @@ def scan(ip):
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.all.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print("IP\t\t\tMAC Address\n------------------------------------------------")
     client_list = []
     for answered_element in answered_list:
         client_dict = {"ip": answered_element[1].psrc, "mac": answered_element[1].hwsrc}
         client_list.append(client_dict)
-        # print(answered_element[1].psrc + "\t\t" + answered_element[1].hwsrc)
-    print(client_list)
+    return client_list
 
 
-scan("192.168.2.1/24")
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n------------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
 
+
+scan_result = scan("192.168.1.1/24")
+print_result(scan_result)
